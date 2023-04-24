9e5fd5fe810eb50b4e0d0bce52f3357fd00250a5
Cs Fahad <csfahadofficiall@gmail.com>
2023-04-24 12:48:30 +0530

    Discord Bot NodeJS

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  message.reply("Hello from csfahad'sBot");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
